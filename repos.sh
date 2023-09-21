4b2f386500078f995abc78f7ba9133891d2e1d71
jcalder0n <117284270+jcalder0n@users.noreply.github.com>
2023-09-21 00:25:21 -0400

    repos.sh
    Este script actualiza los repositorios de Loc-OS 22 y de Loc-OS 23 de forma simple.

--- repos.sh
+++ repos.sh
@@ -1,37 +1,125 @@
 #!/bin/bash
 
-echo "# Official Loc-OS mirrors" > /etc/apt/sources.list.d/loc-os.list
+echo "¿Estás usando Loc-OS 22 o Loc-OS 23?"
+echo "1 Loc-OS 22"
+echo "2 Loc-OS 23"
+echo ""
+echo "Seleccione una:"
+read version
 
-echo "# CDN" >> /etc/apt/sources.list.d/loc-os.list
+clear
 
-echo "#deb https://cdn-br.locosporlinux.com contutti main" >> /etc/apt/sources.list.d/loc-os.list
-echo "deb https://cdn.locosporlinux.com contutti main" >> /etc/apt/sources.list.d/loc-os.list
-echo "deb https://bunny.locosporlinux.com contutti main" >> /etc/apt/sources.list.d/loc-os.list
+if [ "$version" -eq 2 ]; then
+    cat <<EOF > /etc/apt/sources.list.d/loc-os.list
+# Official Loc-OS mirrors
+# CDN
+#deb https://cdn-br.locosporlinux.com contutti main
+deb https://cdn.locosporlinux.com contutti main
+deb https://bunny.locosporlinux.com contutti main
+#deb https://bunny-br.locosporlinux.com contutti main
+deb https://cloud.locosporlinux.com contutti main
+# USA
+deb http://us.locosporlinux.com contutti main
+deb http://us2.locosporlinux.com contutti main
+deb http://deb.locosporlinux.com contutti main
+# MEXICO
+#deb http://mx.locosporlinux.com contutti main
+# BRAZIL
+#deb http://br1.locosporlinux.com contutti main
+#deb http://br2.locosporlinux.com contutti main
+#deb http://br.locosporlinux.com contutti main
+# SPAIN
+#deb http://es.locosporlinux.com contutti main
+#deb http://es2.locosporlinux.com contutti main
+# GERMANY
+#deb http://gr.locosporlinux.com contutti main
+# FRANCE
+#deb http://fr.locosporlinux.com contutti main
+EOF
 
-echo "#deb https://bunny-br.locosporlinux.com contutti main" >> /etc/apt/sources.list.d/loc-os.list
-echo "deb https://cloud.locosporlinux.com contutti main" >> /etc/apt/sources.list.d/loc-os.list
+    echo "Se ha actualizado de manera correcta."
+    echo ""
+    echo "Quiere configurar los mirrors (y/N)?"
+    read respuesta
 
-echo "#USA" >> /etc/apt/sources.list.d/loc-os.list
-echo "deb http://us.locosporlinux.com contutti main" >> /etc/apt/sources.list.d/loc-os.list
-echo "deb http://us2.locosporlinux.com contutti main" >> /etc/apt/sources.list.d/loc-os.list
-echo "deb http://deb.locosporlinux.com contutti main" >> /etc/apt/sources.list.d/loc-os.list
+    if [ "$respuesta" == "y" ]; then
+        clear
+        echo "BRAZIL --- 1"
+        echo "MEXICO --- 2"
+        echo "FRANCE --- 3"
+        echo "SPAIN ---- 4"
+        echo "GERMANY -- 5"
+        echo ""
+        read mirror
 
-echo "#MEXICO" >> /etc/apt/sources.list.d/loc-os.list
-echo "#deb http://mx.locosporlinux.com contutti main" >> /etc/apt/sources.list.d/loc-os.list
+        case $mirror in
+        1)
+            # BRAZIL
+            sed -i 's/#deb http:\/\/br1.locosporlinux.com contutti main/deb http:\/\/br1.locosporlinux.com contutti main/' /etc/apt/sources.list.d/loc-os.list
+            sed -i 's/#deb http:\/\/br2.locosporlinux.com contutti main/deb http:\/\/br2.locosporlinux.com contutti main/' /etc/apt/sources.list.d/loc-os.list
+            sed -i 's/#deb http:\/\/br.locosporlinux.com contutti main/deb http:\/\/br.locosporlinux.com contutti main/' /etc/apt/sources.list.d/loc-os.list
+            ;;
+        2)
+            # MEXICO
+            sed -i 's/#deb http:\/\/mx.locosporlinux.com contutti main/deb http:\/\/mx.locosporlinux.com contutti main/' /etc/apt/sources.list.d/loc-os.list
+            ;;
+        3)
+            # FRANCE
+            sed -i 's/#deb http:\/\/fr.locosporlinux.com contutti main/deb http:\/\/fr.locosporlinux.com contutti main/' /etc/apt/sources.list.d/loc-os.list
+            ;;
+        4)
+            # SPAIN
+            sed -i 's/#deb http:\/\/es.locosporlinux.com contutti main/deb http:\/\/es.locosporlinux.com contutti main/' /etc/apt/sources.list.d/loc-os.list
+            sed -i 's/#deb http:\/\/es2.locosporlinux.com contutti main/deb http:\/\/es2.locosporlinux.com contutti main/' /etc/apt/sources.list.d/loc-os.list
+            ;;
+        5)
+            # GERMANY
+            sed -i 's/#deb http:\/\/gr.locosporlinux.com contutti main/deb http:\/\/gr.locosporlinux.com contutti main/' /etc/apt/sources.list.d/loc-os.list
+            ;;
+        *)
+            clear
+            echo "Opción inválida"
+            echo "Configurados por defecto"
+            echo ""
+            cat /etc/apt/sources.list.d/loc-os.list
+            ;;
+        esac
+        clear
+        echo "Operación exitosa."
+        echo ""
+        echo "Repositorios en /etc/apt/sources.list.d/loc-os.list"
+        echo ""
+        cat /etc/apt/sources.list.d/loc-os.list
+    else
+        clear
+        echo "Operación exitosa, configurados por defecto."
+        echo "Repositorios en /etc/apt/sources.list.d/loc-os.list"
+        echo ""
+        cat /etc/apt/sources.list.d/loc-os.list
+    fi
 
-echo "#BRAZIL" >> /etc/apt/sources.list.d/loc-os.list
-echo "#deb http://br1.locosporlinux.com contutti main" >> /etc/apt/sources.list.d/loc-os.list
-echo "#deb http://br2.locosporlinux.com contutti main" >> /etc/apt/sources.list.d/loc-os.list
-echo "#deb http://br.locosporlinux.com contutti main" >> /etc/apt/sources.list.d/loc-os.list
+elif [ "$version" -eq 1 ]; then
+    cat <<EOF > /etc/apt/sources.list.d/loc-os22.list
+deb http://us.locosporlinux.com elloco main
+deb http://us2.locosporlinux.com elloco main
+deb http://deb.locosporlinux.com elloco main
+deb http://br1.locosporlinux.com elloco main
+deb http://br2.locosporlinux.com elloco main
+deb https://cdn-br.locosporlinux.com elloco main
+deb https://cdn.locosporlinux.com elloco main
+deb https://bunny.locosporlinux.com elloco main
+deb https://bunny-br.locosporlinux.com elloco main
+deb https://cloud.locosporlinux.com elloco main
+deb http://es.locosporlinux.com elloco main
+deb http://es2.locosporlinux.com elloco main
+EOF
 
-echo "#SPAIN" >> /etc/apt/sources.list.d/loc-os.list
-echo "#deb http://es.locosporlinux.com contutti main" >> /etc/apt/sources.list.d/loc-os.list
-echo "#deb http://es2.locosporlinux.com contutti main" >> /etc/apt/sources.list.d/loc-os.list
-
-echo "#GERMANY" >> /etc/apt/sources.list.d/loc-os.list
-echo "#deb http://gr.locosporlinux.com contutti main" >> /etc/apt/sources.list.d/loc-os.list
-
-echo "#FRANCE" >> /etc/apt/sources.list.d/loc-os.list
-echo "#deb http://fr.locosporlinux.com contutti main" >> /etc/apt/sources.list.d/loc-os.list
-
-rm /etc/apt/sources.list.d/loc-os22.list
+    echo "Operación exitosa."
+    echo ""
+    echo "Repositorios en /etc/apt/sources.list.d/loc-os.list"
+    echo ""
+    cat /etc/apt/sources.list.d/loc-os22.list
+else
+    clear
+    echo "Ha ingresado una opción inválida."
+fi
